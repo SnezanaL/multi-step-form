80f7027372447e6fd8aebfb080655d4343a6d299
SnezanaL <snezanalalatovic@gmail.com>
2024-03-10 09:38:07 +0100

    fixed footer

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,26 @@ const monthly = document.getElementById("monthly") as HTMLElement;
 const regForm = document.getElementById("regForm") as HTMLFormElement;
 
 const step2summary = document.getElementById("summary__step2") as HTMLElement;
+const changePlan = document.getElementById("changePlan") as HTMLElement;
+
+const addOnPrice = document.getElementsByClassName("add-on__price");
+
+const addOnlineService = document.getElementById(
+  "onlineService"
+) as HTMLInputElement;
+
+const addCustomizableProfile = document.getElementById(
+  "customizableProfile"
+) as HTMLInputElement;
+const addLargerStorage = document.getElementById(
+  "largerStorage"
+) as HTMLInputElement;
+const summaryTitle = document.getElementById("summary__title") as HTMLElement;
+
+const summaryPrice = document.getElementById("summary__price") as HTMLElement;
+const totalTitle = document.getElementById("totalTitle") as HTMLElement;
+const totalSum = document.getElementById("totalSum") as HTMLElement;
+let formAddons = [] as any;
 
 interface PriceLookup {
   monthly: {
@@ -96,159 +116,36 @@ let planPrice = 0;
 
 showTab(currentStep); // Display the current tab
 
-function sumTotal(planPrice: number, addons: number) {
-  if (addons === undefined) {
-    addons = 0;
-  }
-  return planPrice + addons;
-}
-
 function showTab(n: number) {
-  boxContent[n].style.display = "block";
+  console.log("🚀 ~ showTab ~ n", n);
+  if (n == 4) {
+    boxContent[4].style.display = "flex";
+  } else {
+    boxContent[n].style.display = "block";
+  }
+
   if (n == 0) {
     prevButton.style.display = "none";
     nextButton.addEventListener("click", (e) => {
-      formValues = {
-        name: inputName.value,
-        email: inputEmail.value,
-        phone: inputPhone.value,
-        option: "",
-        plan: "",
-        addons: [] as string[],
-      };
+      e.preventDefault();
+      step1(inputName.value, inputEmail.value, inputPhone.value);
     });
   } else {
     prevButton.style.display = "inline";
   }
   if (n == 1) {
     nextButton.addEventListener("click", (e) => {
-      formValues.option = "";
-      formValues.plan = "";
-      const cardSelected = document.getElementsByClassName("selected");
-      formValues.option = cardSelected[0].id;
-      formValues.plan = checkBox.checked ? "yearly" : "monthly";
-      formValues = {
-        ...formValues,
-        option: cardSelected[0].id,
-        plan: checkBox.checked ? "yearly" : "monthly",
-      };
+      e.preventDefault();
+      step2();
+      addOnPlan(formValues.plan);
     });
   }
 
   if (n == 2) {
-    const addOnlineService = document.getElementById(
-      "onlineService"
-    ) as HTMLInputElement;
-
-    const addCustomizableProfile = document.getElementById(
-      "customizableProfile"
-    ) as HTMLInputElement;
-    const addLargerStorage = document.getElementById(
-      "largerStorage"
-    ) as HTMLInputElement;
-    let formAddons = [] as any;
-
     nextButton.addEventListener("click", (e) => {
-      formAddons = [];
-      if (addOnlineService.checked) {
-        formAddons.push(addOnlineService.id);
-      }
-      if (addCustomizableProfile.checked) {
-        formAddons.push(addCustomizableProfile.id);
-      }
-      if (addLargerStorage.checked) {
-        formAddons.push(addLargerStorage.id);
-      }
-      console.log("🚀 ~ showTab ~ addOnlineService:", addOnlineService);
-      console.log("🚀 ~ showTab ~ formAddons:", formAddons);
-      formValues = {
-        ...formValues,
-        addons: [formAddons],
-      };
-
-      const summary = document.getElementsByClassName(
-        "summary"
-      ) as HTMLCollectionOf<HTMLElement>;
-
-      const summaryTitle = document.getElementById(
-        "summary__title"
-      ) as HTMLElement;
-      const summaryPlan = document.getElementById(
-        "summary__plan"
-      ) as HTMLElement;
-      const summaryPrice = document.getElementById(
-        "summary__price"
-      ) as HTMLElement;
-      const totalTitle = document.getElementById("totalTitle") as HTMLElement;
-      const totalSum = document.getElementById("totalSum") as HTMLElement;
-      const selectedOption = formValues.option as "arcade" | "advanced" | "pro";
-
-      const selectedPlan = formValues.plan as "monthly" | "yearly";
-      const priceText: string = PriceLookup[selectedPlan].text;
-      const price = PriceLookup[selectedPlan][selectedOption];
-      if (
-        PriceLookup[selectedPlan] &&
-        PriceLookup[selectedPlan][selectedOption] !== undefined
-      ) {
-        planPrice = PriceLookup[selectedPlan][selectedOption];
-      } else {
-        // Handle invalid plan or option value
-        console.error("Invalid plan or option value");
-      }
-
-      summaryTitle.innerHTML = `${
-        selectedOption.charAt(0).toUpperCase() + selectedOption.slice(1)
-      } (${selectedPlan.charAt(0).toUpperCase() + selectedPlan.slice(1)})`; // Capitalize the first letter
-      summaryPrice.innerHTML = `+${planPrice}/${priceText}`;
-
-      let template = "";
-      let step2summaryArray = [];
-      let addonPrices = 0;
-      let total = 0;
-      let noAddons = "";
-
-      console.log(
-        "🚀 ~ nextButton.addEventListener ~ formValues.addons:",
-        formValues.addons
-      );
-
-      for (const item of formValues.addons) {
-        if (formValues.addons[0].length === 0) {
-          noAddons = `
-          <div class="summary__step2__group"> 
-          <p class="summary__title">No Addons</p>
-          <p class="summary__price">+$0/${priceText}</p>
-          </div>`;
-        } else {
-          const itemTitle = (Addons.title as { [key: string]: string })[
-            item
-          ] as "onlineService" | "customizableProfile" | "largerStorage";
-
-          const price = (Addons[selectedPlan] as { [key: string]: number })[
-            item
-          ];
-          template = ` <div class="summary__step2__group">
-          <p class="summary__title">${itemTitle}</p>
-          <p class="summary__price">+${price}/${priceText}</p>
-         
-          </div>`;
-          addonPrices += price;
-          step2summaryArray.push(template);
-        }
-
-        console.log("🚀 ~ nextButton.addEventListener ~ noAddons:", noAddons);
-        step2summary.innerHTML =
-          formValues.addons[0].length === 0
-            ? noAddons
-            : step2summaryArray.join("");
-        console.log(
-          "🚀 ~ nextButton.addEventListener ~ step2summary.innerHTML:",
-          step2summary.innerHTML
-        );
-        total = sumTotal(planPrice, addonPrices);
-        totalTitle.innerHTML = `Total (per ${selectedPlan})`;
-        totalSum.innerHTML = ` +$${total}/${priceText}`;
-      }
+      // e.preventDefault();
+      step3();
+      showPlan(formValues.plan, formValues.option, formValues.addons[0]);
     });
   }
 
@@ -466,5 +363,142 @@ if (checkBox) {
 }
 
 /* ----------------------------------- */
-// #### Count TOTAL  ########
+// ####  Step 3 ########
 /* ----------------------------------- */
+const addOnPlan = (plan: string) => {
+  if (plan === "yearly") {
+    addOnPrice[0].innerHTML = "+$10/yr";
+    addOnPrice[1].innerHTML = "+$20/yr";
+    addOnPrice[2].innerHTML = "+$20/yr";
+  } else {
+    addOnPrice[0].innerHTML = "+$1/mo";
+    addOnPrice[1].innerHTML = "+$2/mo";
+    addOnPrice[2].innerHTML = "+$2/mo";
+  }
+};
+
+/* ----------------------------------- */
+// #### Functions  ########
+/* ----------------------------------- */
+const step1 = (name: string, email: string, phone: string) => {
+  formValues = {
+    name: "",
+    email: "",
+    phone: "",
+    option: "",
+    plan: "",
+    addons: [] as string[],
+  };
+  formValues = {
+    name: name,
+    email: email,
+    phone: phone,
+    option: "",
+    plan: "",
+    addons: [] as string[],
+  };
+};
+
+const step2 = () => {
+  formValues.option = "";
+  formValues.plan = "";
+  const cardSelected = document.getElementsByClassName("selected");
+  formValues.option = cardSelected[0].id;
+  formValues.plan = checkBox.checked ? "yearly" : "monthly";
+  formValues = {
+    ...formValues,
+    option: cardSelected[0].id,
+    plan: checkBox.checked ? "yearly" : "monthly",
+  };
+};
+
+const step3 = () => {
+  formAddons = [];
+  if (addOnlineService.checked) {
+    formAddons.push(addOnlineService.id);
+  }
+  if (addCustomizableProfile.checked) {
+    formAddons.push(addCustomizableProfile.id);
+  }
+  if (addLargerStorage.checked) {
+    formAddons.push(addLargerStorage.id);
+  }
+  formValues = {
+    ...formValues,
+    addons: [formAddons],
+  };
+
+  return formValues;
+};
+
+const showPlan = (plan: string, option: string, addons: any) => {
+  const selectedOption = option as "arcade" | "advanced" | "pro";
+  const selectedPlan = plan as "monthly" | "yearly";
+  const priceText: string = PriceLookup[selectedPlan].text;
+  if (
+    PriceLookup[selectedPlan] &&
+    PriceLookup[selectedPlan][selectedOption] !== undefined
+  ) {
+    planPrice = PriceLookup[selectedPlan][selectedOption];
+  } else {
+    // Handle invalid plan or option value
+    console.error("Invalid plan or option value");
+  }
+
+  summaryTitle.innerHTML = `${
+    selectedOption.charAt(0).toUpperCase() + selectedOption.slice(1)
+  } (${selectedPlan.charAt(0).toUpperCase() + selectedPlan.slice(1)})`; // Capitalize the first letter
+  summaryPrice.innerHTML = `+${planPrice}/${priceText}`;
+  let template = "";
+  let step2summaryArray = [];
+  let addonPrices = 0;
+  let total = 0;
+  let noAddons = "";
+  console.log("🚀 ~ showPlan ~ addons:", addons);
+
+  if (addons.length === 0) {
+    noAddons = `
+      <div class="summary__step2__group"> 
+      <p class="summary__title">No Addons</p>
+      <p class="summary__price">+$0/${priceText}</p>
+      </div>`;
+    console.log("🚀 ~ showPlan ~ noAddons:", noAddons);
+  } else {
+    for (const item of addons) {
+      const itemTitle = (Addons.title as { [key: string]: string })[item] as
+        | "onlineService"
+        | "customizableProfile"
+        | "largerStorage";
+
+      const price = (Addons[selectedPlan] as { [key: string]: number })[item];
+      template = ` <div class="summary__step2__group">
+      <p class="summary__title">${itemTitle}</p>
+      <p class="summary__price">+${price}/${priceText}</p>
+     
+      </div>`;
+      addonPrices += price;
+      step2summaryArray.push(template);
+    }
+  }
+  step2summary.innerHTML =
+    addons.length === 0 ? noAddons : step2summaryArray.join("");
+  console.log(
+    "🚀 ~ showPlan ~ step2summary.innerHTML:",
+    step2summary.innerHTML
+  );
+  total = sumTotal(planPrice, addonPrices);
+  totalTitle.innerHTML = `Total (per ${selectedPlan})`;
+  totalSum.innerHTML = ` +$${total}/${priceText}`;
+};
+
+const sumTotal = (planPrice: number, addons: number) => {
+  if (addons === undefined) {
+    addons = 0;
+  }
+  return planPrice + addons;
+};
+
+changePlan.addEventListener("click", function (e) {
+  e.preventDefault();
+  nextPrev(-2);
+});
